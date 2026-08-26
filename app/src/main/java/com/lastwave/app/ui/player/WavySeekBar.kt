56b97b8ae362886a0ba6849401b48bee8fcc1a02
ajisth69 <ajisth69@users.noreply.github.com>
2026-08-26 18:33:08 +0530

    fix(seekbar): remove unresolved matchParentSize import

--- app/src/main/java/com/lastwave/app/ui/player/WavySeekBar.kt
+++ app/src/main/java/com/lastwave/app/ui/player/WavySeekBar.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
