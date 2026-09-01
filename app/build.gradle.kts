4cf57c427c0485c3c1727bec0105092766d8acbe
ajisth69 <ajisth69@users.noreply.github.com>
2026-09-01 12:13:56 +0530

    fix(build): force Kotlin 1.9.24 stdlib and coroutines 1.8.1 dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -285,7 +285,17 @@ dependencies {
 
 configurations.all {
     resolutionStrategy {
-        force("io.github.dokar3:quickjs-kt-android:1.0.12")
+        force(
+            "org.jetbrains.kotlin:kotlin-stdlib:1.9.24",
+            "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.24",
+            "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.24",
+            "org.jetbrains.kotlin:kotlin-stdlib-common:1.9.24",
+            "org.jetbrains.kotlin:kotlin-reflect:1.9.24",
+            "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1",
+            "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1",
+            "org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.3",
+            "org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3",
+        )
     }
 }
 
